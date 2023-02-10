9dd71fa2788ce1a2a57f5a0744cf5d26dea93e4e
Artur Cavalcante <artursousac@hotmail.com>
2023-02-09 23:02:36 -0300

    Adicionando novos exercícios para a pasta

---
+++ Ex87 - Matriz em Python 2.py
@@ -0,0 +1,3 @@
+# Exercício 87. Aprimore o desafio anterior, mostrando no final: A) A soma de todos os valores pares digitados. B) A soma dos valores da terceira coluna. C) O maior valor da segunda linha.
+
+
