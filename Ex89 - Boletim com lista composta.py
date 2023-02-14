3ab2b710c6c1af4dff02d356bcfe8977c1e1fda4
Artur Cavalcante <artursousac@hotmail.com>
2023-02-14 12:55:15 -0300

    Adiciona o exercício 89

--- Ex89 - Boletim com lista composta.py
+++ Ex89 - Boletim com lista composta.py
@@ -1,4 +1,6 @@
 # Exercício 89. Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita
 # que o usuário possa mostrar as notas de cada aluno individualmente.
 
+print('\033[1;31mENTRADAS DO USUÁRIO\033[m')
+
 
